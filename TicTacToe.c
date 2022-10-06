346a7670d118e010cc163bfc8121e6527ee35af7
dcjincpp <112600779+dcjincpp@users.noreply.github.com>
2022-10-05 19:21:57 -0700

    Outline of code using comments

---
+++ TicTacToe.c
@@ -0,0 +1,22 @@
+//Tic Tac Toe game between 2 players or player vs computer
+
+
+int main(){
+    //prompt user for game they wish to play (player or computer)
+
+
+    //read information from console
+
+/* repeat until win or tie
+    //show empty board (current status)
+
+
+    //prompt user to make their move by typing
+*/
+    
+
+
+
+
+    return 0;
+}
